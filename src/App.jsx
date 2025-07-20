c754f8213bb274c81cda7866eb4bfd4785645545
Trckmazz <dylanprinsloo@icloud.com>
2025-07-20 15:53:51 +0200

    tsconfig

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './static/css/_common.scss';
+import './index.css'
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Topbar from './components/Topbar/Topbar';
 import Home from './pages/Home';
 import Footer from './components/Footer/Footer';
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Topbar />
       <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home" element={<Home />} />
       </Routes>
       <Footer />
