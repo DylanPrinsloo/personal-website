7d4fcafa6d6421ee019ed4140110b51a573d50a6
Trckmazz <dylanprinsloo@icloud.com>
2025-09-09 16:15:33 +0200

    next-config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: false,
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
   },
+  basePath: '/personal-website',
+  assetPrefix: '/personal-website/',
 };
 
 export default nextConfig;
